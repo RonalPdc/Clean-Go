342ea2e7d1f259b2cf0a879f4fd90cb6cfb8e4d8
Ronal <Ronal@ASUSROND>
2026-07-09 15:47:29 -0400

    Agregar archivos de proyecto.

---
+++ Clean&Go-BusinessLogic/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clean_Go_BusinessLogic
+{
+    public class Class1
+    {
+    }
+}
---
+++ Clean&Go-Entities/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clean_Go_Entities
+{
+    public class Class1
+    {
+    }
+}
