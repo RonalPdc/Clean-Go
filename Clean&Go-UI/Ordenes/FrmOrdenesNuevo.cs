00e5095b097143441f73b69e9136faa82d418fc8
Ronal <ronal@example.com>
2026-07-15 04:34:16 -0400

    Reordenar asignaciones de autocompletado en FrmOrdenesNuevo.cs para evitar excepcion de DropDownList

--- Clean&Go-UI/Ordenes/FrmOrdenesNuevo.cs
+++ Clean&Go-UI/Ordenes/FrmOrdenesNuevo.cs
@@ -58,9 +58,9 @@ namespace Clean_Go.BusinessLogic.Ordenes
                 txtObservacionesDetalle.MaxLength = 100;
                 txtCantidad.MaxLength = 3;
 
+                cmbCliente.DropDownStyle = ComboBoxStyle.DropDown;
                 cmbCliente.AutoCompleteMode = AutoCompleteMode.SuggestAppend;
                 cmbCliente.AutoCompleteSource = AutoCompleteSource.ListItems;
-                cmbCliente.DropDownStyle = ComboBoxStyle.DropDown;
 
                 CargarCombos();
                 txtNumeroOrden.Text = "ORD-" + DateTime.Now.ToString("yyMMddHHmmss");
