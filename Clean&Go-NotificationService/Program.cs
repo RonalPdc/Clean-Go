2cff7334e6a7d35df23ce75ff7ea48a460ac88e8
Ronal <ronal@example.com>
2026-07-15 16:59:38 -0400

    Restaurar OutputType a WinExe para funcionamiento normal de Windows Service y agregar AllocConsole nativo en Program.cs

--- Clean&Go-NotificationService/Program.cs
+++ Clean&Go-NotificationService/Program.cs
@@ -1,14 +1,19 @@
 using System;
 using System.ServiceProcess;
+using System.Runtime.InteropServices;
 
 namespace Clean_Go_NotificationService
 {
     class Program
     {
+        [DllImport("kernel32.dll")]
+        private static extern bool AllocConsole();
+
         static void Main(string[] args)
         {
             if (Environment.UserInteractive)
             {
+                AllocConsole();
                 CleanGoService servicio = new CleanGoService();
                 servicio.IniciarConsola();
                 Console.WriteLine("Servicio de Notificaciones Clean&Go corriendo en modo interactivo.");
